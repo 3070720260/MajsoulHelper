8a9e99aa572a83a5213a38925e2d0d41ee16e257
BUGJiang <60597605+3070720260@users.noreply.github.com>
2025-08-26 20:47:35 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,7 +12,7 @@ case "$1" in
         cd /app/MajsoulMax
         # 获取 upstream 环境变量，默认 http://akagi:7880
         upstream=${UPSTREAM:-http://akagi:7880}
-        exec mitmdump -p 23410 --mode upstream:"$upstream" -s addons.py --ssl-insecure
+        exec mitmdump -p 23410 --mode upstream:"$upstream" -s addons.py --ssl-insecure --set block_global=false
         ;;
     *)
         exec "$@"
